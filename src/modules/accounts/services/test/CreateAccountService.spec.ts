6ff84bd00ec9fb43cb2c88c48e7e20609da1756b
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-12-23 15:04:23 -0300

    chore: resolve jest configs

--- src/modules/accounts/services/test/CreateAccountService.spec.ts
+++ src/modules/accounts/services/test/CreateAccountService.spec.ts
@@ -1,25 +1,98 @@
+import AppError from '@shared/errors/AppError';
 import FakeAccountsRepository from '../../repositories/fakes/FakeAccountsRepository';
 import FakeAccountTypesRepository from '../../repositories/fakes/FakeAccountTypesRepository';
 import CreateAccountService from '../CreateAccountService';
 
+let fakeAccountsRepository: FakeAccountsRepository;
+let fakeAccountTypesRepository: FakeAccountTypesRepository;
+let createAccount: CreateAccountService;
+
 describe('CreateAccount', () => {
-  const fakeAccountsRepository = new FakeAccountsRepository();
-  const fakeAccountTypesRepository = new FakeAccountTypesRepository();
-  it('should be able to create a new account', async () => {
-    const createAccount = new CreateAccountService(
+  beforeEach(() => {
+    fakeAccountsRepository = new FakeAccountsRepository();
+    fakeAccountTypesRepository = new FakeAccountTypesRepository();
+    createAccount = new CreateAccountService(
       fakeAccountsRepository,
       fakeAccountTypesRepository,
     );
+  });
+
+  it('should be able to create a new account', async () => {
+    await fakeAccountTypesRepository.create({
+      user_id: '123123',
+      name: 'accountType-test',
+    });
 
     const account = await createAccount.execute({
-      name: 'conta-teste',
+      name: 'account-test',
       user_id: '123123',
-      account_type: '123123',
+      account_type: 'accountType-test',
     });
 
     expect(account).toHaveProperty('id');
-    expect(account.name).toBe('conta-teste');
+    expect(account.name).toBe('account-test');
     expect(account.user_id).toBe('123123');
-    expect(account.account_type).toBe('123123');
+  });
+
+  it('should not be able to create two accounts with the same name to the same user', async () => {
+    const user1 = '123123';
+
+    await fakeAccountTypesRepository.create({
+      user_id: user1,
+      name: 'account-type',
+    });
+
+    await createAccount.execute({
+      name: 'account-1',
+      user_id: user1,
+      account_type: 'account-type',
+    });
+
+    await expect(
+      createAccount.execute({
+        name: 'account-1',
+        user_id: user1,
+        account_type: 'account-type',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create accounts with the same name for different users', async () => {
+    // create one account-type for each fake user
+    await fakeAccountTypesRepository.create({
+      user_id: 'user-1',
+      name: 'account-type',
+    });
+
+    await fakeAccountTypesRepository.create({
+      user_id: 'user-2',
+      name: 'account-type',
+    });
+
+    // create one fake account for the first fake user
+    await createAccount.execute({
+      name: 'account',
+      user_id: 'user-1',
+      account_type: 'account-type',
+    });
+
+    // expect that other user can create an account with the same name of the first user
+    await expect(
+      createAccount.execute({
+        name: 'account',
+        user_id: 'user-2',
+        account_type: 'account-type',
+      }),
+    ).resolves.toHaveProperty('id');
+  });
+
+  it('should not be able to create an account with an invalid account-type', async () => {
+    await expect(
+      createAccount.execute({
+        name: 'account',
+        user_id: 'user-1',
+        account_type: 'invalid-account-type',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
