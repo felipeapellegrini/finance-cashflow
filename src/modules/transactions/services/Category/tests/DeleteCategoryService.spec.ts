507b6e423f07af1ea795929ad2b3ef39fc0ff9e6
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-12-24 13:49:53 -0300

    test: ensure can delete a category

---
+++ src/modules/transactions/services/Category/tests/DeleteCategoryService.spec.ts
@@ -0,0 +1,31 @@
+// import AppError from '@shared/errors/AppError';
+import FakeCategoriesRepository from '../../../repositories/fakes/FakeCategoriesRepository';
+import DeleteCategoryService from '../services/DeleteCategoryService';
+
+let fakeCategoriesRepository: FakeCategoriesRepository;
+let deleteCategory: DeleteCategoryService;
+
+describe('DeleteCategory', () => {
+  beforeEach(() => {
+    fakeCategoriesRepository = new FakeCategoriesRepository();
+    deleteCategory = new DeleteCategoryService(fakeCategoriesRepository);
+  });
+  it('should be able to delete a category', async () => {
+    const category = await fakeCategoriesRepository.create({
+      name: 'category',
+      user_id: 'user',
+    });
+
+    await deleteCategory.execute({
+      user_id: 'user',
+      id: category.id,
+    });
+
+    const categories = await fakeCategoriesRepository.findById({
+      user_id: 'user',
+      id: category.id,
+    });
+
+    expect(categories).toBeUndefined();
+  });
+});
