687e6dd27a4895ead6b1731cec12aad0647bf45d
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-12-24 15:46:26 -0300

    test: should be able to update a category name

---
+++ src/modules/transactions/services/Category/tests/UpdateCategoryService.spec.ts
@@ -0,0 +1,28 @@
+// import AppError from '@shared/errors/AppError';
+import FakeCategoriesRepository from '../../../repositories/fakes/FakeCategoriesRepository';
+import UpdateCategoryService from '../services/UpdateCategoryService';
+
+let fakeCategoriesRepository: FakeCategoriesRepository;
+let updateCategories: UpdateCategoryService;
+
+describe('UpdateCategories', () => {
+  beforeEach(() => {
+    fakeCategoriesRepository = new FakeCategoriesRepository();
+    updateCategories = new UpdateCategoryService(fakeCategoriesRepository);
+  });
+  it('should be able to update a category name', async () => {
+    const category = await fakeCategoriesRepository.create({
+      name: 'category',
+      user_id: 'user',
+    });
+
+    const newCategory = await updateCategories.execute({
+      user_id: 'user',
+      category_id: category.id,
+      category_name: 'new category',
+    });
+
+    expect(newCategory.name).toBe('new category');
+    expect(newCategory.id).toEqual(category.id);
+  });
+});
