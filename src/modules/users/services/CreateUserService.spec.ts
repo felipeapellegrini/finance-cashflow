ed0e6b45f261db18cd8573da9adfb5a07c22d6a9
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-12-26 10:26:51 -0300

    test: should be able to create a user

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,24 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+  });
+  it('should be able to create a user', async () => {
+    const user = await createUser.execute({
+      name: '',
+      email: '',
+      password: '',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+});
