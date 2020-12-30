456f4012e8138edd1fb55d7b149f5d594270264b
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-12-29 23:55:39 -0300

    test: should be able to authenticate user

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,37 @@
+// import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+let fakeHashProvier: FakeHashProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let authenticate: AuthenticateUserService;
+let createUser: CreateUserService;
+
+describe('AuthenticateUser', () => {
+  beforeEach(() => {
+    fakeHashProvier = new FakeHashProvider();
+    fakeUsersRepository = new FakeUsersRepository();
+    authenticate = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvier,
+    );
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvier);
+  });
+  it('should be able to authenticate an user', async () => {
+    const user = await createUser.execute({
+      name: 'user',
+      email: 'user@mail.com',
+      password: '123456',
+    });
+
+    const response = await authenticate.execute({
+      email: 'user@mail.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toBe(user);
+  });
+});
