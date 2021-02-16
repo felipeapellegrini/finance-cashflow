2b1830ca8b60e75c6a92d315a87fd1117204a20f
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-02-16 13:09:43 -0300

    feat: add transactionsRouter which calls TransactionsController

---
+++ src/modules/transactions/infra/http/controllers/TransactionsController.ts
@@ -0,0 +1,44 @@
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateTransactionService from '@modules/transactions/services/Transaction/services/CreateTransactionService';
+
+export default class TransactionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    try {
+      const user_id = request.user.id;
+      const {
+        account_id,
+        subcategory_id,
+        costcenter_id,
+        installment,
+        installments,
+        transaction_date,
+        due_date,
+        payment_date,
+        total,
+        description,
+      } = request.body;
+
+      const createTransaction = container.resolve(CreateTransactionService);
+
+      const transaction = await createTransaction.execute({
+        user_id,
+        account_id,
+        subcategory_id,
+        costcenter_id,
+        installment,
+        installments,
+        transaction_date,
+        due_date,
+        payment_date,
+        total,
+        description,
+      });
+
+      return response.json(transaction);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+}
